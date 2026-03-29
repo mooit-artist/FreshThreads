bce2525373a949f2ec0540aa1e0e20c023e0f61d
Bryan Jorgensen <bryanjorgensen@wks-macstudio-01.user.lan>
2026-03-29 18:01:52 -0500

    Strip to irreplaceable assets only - design images, logos, space-cosmic-explorer

--- docker/start-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Simple start script for nginx (removing OpenAppSec for now)
-echo "Starting FreshThreads frontend..."
-
-# Start nginx
-exec nginx -g "daemon off;"
--- scripts/check-users.ps1
+++
@@ -1,5 +0,0 @@
-# Check existing users in M365 tenant
-Connect-MicrosoftTeams
-Get-CsOnlineUser | Select-Object DisplayName, UserPrincipalName | Format-Table -AutoSize
-Disconnect-MicrosoftTeams
-
